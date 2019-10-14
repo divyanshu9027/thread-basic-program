b1c7fdcd5d8e6103e3746a50d33b12d75c7e1234
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2019-10-14 08:00:09 -0700

    Thread basic program to print hii divyanshu sharma and hello divyanshu sharma 5 times

---
+++ src/ThreadDemo.java
@@ -0,0 +1,26 @@
+class hii{
+    public void show() {
+        for(int i=1;i<=5;i++) {
+            System.out.println("hii divyanshu sharma");
+        }
+
+    }
+
+}
+class hello {
+    public void show() {
+        for(int i=1;i<=5;i++) {
+            System.out.println("hello divyanshu sharma");
+
+        }
+    }
+}
+public class ThreadDemo {
+    public static void main(String[] args) {
+        hii divyanshu=new hii();
+        hello divyanshu2=new hello();
+        divyanshu.show();
+        divyanshu2.show();
+
+    }
+}
